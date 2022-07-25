7d51e71e87b225a48df471088e66a3fd50e3d330
lajiaomian <lajiaomian@qq.com>
2022-07-25 22:58:40 +0800

    push test master

--- git_demo1/src/main/java/hello.java
+++ git_demo1/src/main/java/hello.java
@@ -6,5 +6,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hi,I am lajioamian!");
         System.out.println("hi,it second times to meet you");
+        System.out.println("push test ----master");
     }
 }
